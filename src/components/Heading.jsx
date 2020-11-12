a3a6b0d73909605c9ae3afaef60d5ed072fd1821
RobBennettUK <38293815+RobBennettUK@users.noreply.github.com>
2020-11-12 13:53:34 +0000

    Lesson Completed

---
+++ src/components/Heading.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+function Heading() {
+  return <h1>My Favourite Foods</h1>;
+}
+
+// Note how we do not use the () as we want to use the output as it is, rather than have to call it
+export default Heading;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,30 @@
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import Heading from "./components/Heading";
+// import List from "./components/List";
+
+// // It is convention to give every function we create a capital letter to start with
+// // This allows the React to differentiate ours from the native HTML
+// // What we actually do, is create a jsx file with the same name as the component and then import it
+// // Remember, in the jsx file we must import React and then export the function without ()
+// // function Heading() {
+// //   return <h1>My Favourite Foods</h1>;
+// // }
+
+// // For best practices, see https://github.com/airbnb/javascript/tree/master/react
+
+// ReactDOM.render(
+//   <div>
+//     <Heading />
+//     <List />
+//   </div>,
+//   document.getElementById("root")
+// );
+
+// What you will see in many websites is no HTML within the HTML, it will just Say
+// There would be a separate App.jsx file that would render all the components and export App as shown in the App.jsx file
 import React from "react";
 import ReactDOM from "react-dom";
+import App from "./components/App";
 
-ReactDOM.render(
-  <div>
-    <h1>My Favourite Foods</h1>
-    <ul>
-      <li>Bacon</li>
-      <li>Jamon</li>
-      <li>Noodles</li>
-    </ul>
-  </div>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
